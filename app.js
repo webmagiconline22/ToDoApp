c5b218b2e14f0c2e74fb98a839ed8152334cf9a3
Dabeer Shaikh <dab8106@gmail.com>
2023-07-30 15:12:54 +0530

    Added a feature of complete and incomplete task

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
-// Wait for the DOM to load before attaching event listeners
 document.addEventListener("DOMContentLoaded", function() {
-    // Get references to the relevant elements
     var taskInput = document.getElementById("taskInput");
     var addTaskBtn = document.getElementById("addTaskBtn");
     var taskList = document.getElementById("taskList");
 
-    // Function to add a task to the list
     function addTask() {
         var taskText = taskInput.value.trim();
 
         if (taskText !== "") {
             var listItem = document.createElement("li");
             var textNode = document.createTextNode(taskText);
+            var checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+
+            listItem.appendChild(checkbox);
             listItem.appendChild(textNode);
             taskList.appendChild(listItem);
             taskInput.value = "";
         }
     }
 
-    // Event listener for the "Add Task" button
     addTaskBtn.addEventListener("click", function() {
         addTask();
     });
 
-    // Event listener for the Enter key press
     taskInput.addEventListener("keyup", function(event) {
         if (event.key === "Enter") {
             addTask();
         }
     });
 
-    // Event delegation for removing tasks
     taskList.addEventListener("click", function(event) {
-        if (event.target.tagName === "LI") {
-            event.target.remove();
+        if (event.target.tagName === "INPUT" && event.target.type === "checkbox") {
+            var listItem = event.target.parentNode;
+            listItem.classList.toggle("completed");
         }
     });
 });
